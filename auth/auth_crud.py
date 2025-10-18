aa5cf0e88e8d8f4b5b36c0beaef0772ac05b5076
Bruce Nyeha <brucenyeha@gmail.com>
2025-10-18 02:09:25 -0700

    Removed duplicate

--- auth/auth_crud.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from app.USER import user_models
-
-def get_user_by_email(db: Session, email: str):
-    user = db.query(user_models.User).filter(user_models.User.email==email).first()
-    return user
--- auth/hashing.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def password_hash(password: str):
-    return pwd_context.hash(password)
-
-def password_verify(plain_password: str, password:str):
-    return pwd_context.verify(plain_password, password)
